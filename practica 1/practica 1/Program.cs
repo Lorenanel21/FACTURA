5b01642aa6374618f04d66cb837693c2f754ee0e
Lorenanl21 <gutty_ves_213@hotmail.com>
2014-10-02 19:59:03 -0500

    ji9  h9ui

--- practica 1/practica 1/Program.cs
+++ practica 1/practica 1/Program.cs
@@ -14,12 +14,48 @@ namespace practica_1
 	{
 		public static void Main(string[] args)
 		{
-			Console.WriteLine("Hello World!");
+			int cantidad1,cantidad2,cantidad3,precio1,precio3;
+			String desc,desc2,desc3;
+			double precio2,iva=7.20;
 			
-			// TODO: Implement Functionality Here
 			
-			Console.Write("Press any key to continue . . . ");
-			Console.ReadKey(true);
+			
+			Console.WriteLine("ingrese cantidad de procucto");
+	     
+		      cantidad1=int.Parse (Console.ReadLine());
+		    Console.WriteLine("ingrese descripcion del producto");
+		      desc=Console.ReadLine();
+		    Console.WriteLine("ingresa precio unitario");
+		     
+		    precio1=int.Parse (Console.ReadLine());
+		    
+		    Console.WriteLine("ingrese cantidad de procucto");
+	     
+		      cantidad2=int.Parse (Console.ReadLine());
+		    Console.WriteLine("ingrese descripcion del producto");
+		      desc2=Console.ReadLine();
+		    Console.WriteLine("ingresa precio unitario");
+		     
+		    precio2=int.Parse (Console.ReadLine());
+		    Console.WriteLine("ingrese cantidad de procucto");
+	     
+		      cantidad3=int.Parse (Console.ReadLine());
+		    Console.WriteLine("ingrese descripcion del producto");
+		      desc3=Console.ReadLine();
+		    Console.WriteLine("ingresa precio unitario");
+		     precio3=int.Parse (Console.ReadLine());
+		     
+		    double cuenta1,cuenta2,cuenta3;
+		    cuenta1=cantidad1+precio1;
+		    cuenta2=cantidad2+precio2;
+		    cuenta3=cantidad3+precio3;
+		    
+		 
+		    Console.WriteLine("el subtotal es",cuenta1+cuenta2+cuenta3);
+		    Console.WriteLine(iva);
+		    Console.WriteLine("EL TOTAL ES:",cuenta1+cuenta2+cuenta3+iva);
+		    Console.Write("precione para salir ");
+		Console.ReadKey(true);
 		}
 	}
 }
